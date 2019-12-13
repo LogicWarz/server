d2d2513558e6c5c6b5401b02b5404b76d83e3c1c
Edison <edirates@gmail.com>
2019-12-13 15:58:25 +0700

    Adding update routes for challenge and authorization for admin to access challenges

--- routes/challenge.js
+++ routes/challenge.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
 const authentication = require("../middlewares/authentication");
+const adminAuthorization = require("../middlewares/adminAuthorization");
 const challengeController = require('../controllers/challenge')
 
 router.use(authentication);
+router.use(adminAuthorization);
 router.post('/', challengeController.create)
 router.get('/', challengeController.getAll)
 router.get('/:id', challengeController.getId)
+router.patch('/:id', challengeController.updateId)
 router.delete('/:id', challengeController.deleteId)
 
 module.exports = router
